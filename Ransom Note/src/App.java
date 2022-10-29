77976a036a3736fc53f04a000df31c21c25c8ab4
My Name <abbdrhm@gmail.com>
2022-10-29 10:54:14 +0300

    false returning value

--- Ransom Note/src/App.java
+++ Ransom Note/src/App.java
@@ -1,5 +1,47 @@
+import java.util.*;
 public class App {
+
+    public static boolean canConstruct(String ransomNote, String magazine){
+        char [] firstWord = ransomNote.toCharArray();
+        char [] secondWord = magazine.toCharArray();
+        int i = 0;
+
+        for(char c : firstWord)
+        {
+            for(char d : secondWord )
+            {
+                if( c == d)
+                {
+
+                  i++;
+                  //System.out.println("found " + c + " " + d + " " + i);
+                  break;  
+                }
+            }
+        }
+        //int c = firstWord.length;
+        if(i == firstWord.length){
+            return true;
+        }
+        else
+            return false;
+       
+
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner sc = new Scanner(System.in)) {
+            String ransomNote = sc.nextLine();
+            String magazine = sc.nextLine();
+         
+            
+            if(canConstruct(ransomNote, magazine)){
+                System.out.println("true");
+            }
+            else
+                System.out.println("false");
+
+
+
+        }
     }
 }
